4316af12d6b49fb6145f677c08f2c28772c0b2d8
unknown <vilfrack21@gmail.com>
2019-12-18 22:34:50 -0300

    Se crea proyecto y se agrega modelo

---
+++ Model/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Model/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model
+{
+    public class Movie : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
